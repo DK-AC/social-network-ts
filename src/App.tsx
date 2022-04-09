557a7e5be446f62b1edc0d4e21e61ff9c41fc6e0
DK-AC <denkacaj@gmail.com>
2022-04-09 17:45:28 +0300

    Router Dom

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import {Header} from "./ui/Header/Header";
 import {Navbar} from "./ui/Navbar/Navbar";
 import {Profile} from "./ui/Profile/Profile";
 import {Dialogs} from "./ui/Dialogs/Dialogs";
+import {Route, Routes} from "react-router-dom";
 
 export const App = () => {
     return (
@@ -11,8 +12,12 @@ export const App = () => {
             <Header/>
             <Navbar/>
             <div className={'app-wrapper-content'}>
-                <Profile/>
-                <Dialogs/>
+                <Routes>
+                    <Route path="/" element={<Profile/>}/>
+                    <Route path="/profile" element={<Profile/>}/>
+                    <Route path="/dialogs" element={<Dialogs/>}/>
+                    <Route path="/*" element={<div>404 error</div>}/>
+                </Routes>
             </div>
         </div>
     );
--- src/ui/Navbar/Navbar.tsx
+++ src/ui/Navbar/Navbar.tsx
@@ -1,14 +1,30 @@
 import React from 'react';
+import {NavLink} from 'react-router-dom';
 import styles from './navbar.module.css'
 
 export const Navbar = () => {
     return (
         <nav className={styles.nav}>
-            <div className={`${styles.item} ${styles.active}`}>Profile</div>
-            <div className={`${styles.item}`}>Messages</div>
-            <div className={`${styles.item}`}>News</div>
-            <div className={`${styles.item}`}>Music</div>
-            <div className={`${styles.item}`}>Settings</div>
+            <div>
+                <NavLink to={'/profile'} className={`${styles.item} ${styles.active}`}>Profile</NavLink>
+            </div>
+            <div>
+                <NavLink to={'/dialogs'} className={`${styles.item}`}>Dialogs</NavLink>
+            </div>
+
+            <div>
+                <NavLink to={'/news'} className={`${styles.item}`}>News</NavLink>
+
+            </div>
+            <div>
+                <NavLink to={'/music'} className={`${styles.item}`}>Music</NavLink>
+
+            </div>
+            <div>
+                <NavLink to={'/settings'} className={`${styles.item}`}>Settings</NavLink>
+
+            </div>
+
         </nav>
     );
 };
