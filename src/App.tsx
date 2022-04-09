aca9c1877da091ecfd8115a308725c86b7d2ddc1
DK-AC <denkacaj@gmail.com>
2022-04-09 13:19:24 +0300

    add ui components

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,13 @@
 import React from 'react';
 import './App.css';
+import {Header} from "./ui/Header/Header";
+import {Footer} from "./ui/Footer/Footer";
 
 export const App = () => {
     return (
-        <div className="App">
-            Social Network
+        <div>
+            <Header/>
+            <Footer/>
         </div>
     );
 }
---
+++ src/ui/Header/Header.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+export const Header = () => {
+    return (
+        <div>
+            <ul>
+                <li><a href="#">Profile</a></li>
+                <li><a href="#">Dialogs</a></li>
+                <li><a href="#">Music</a></li>
+                <li><a href="#">News</a></li>
+                <li><a href="#">Settings</a></li>
+            </ul>
+        </div>
+    );
+};
+
