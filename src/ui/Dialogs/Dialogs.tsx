daf7eb65c677a2b5c85d5c5bf55af53a277e31d2
DK-AC <denkacaj@gmail.com>
2022-04-10 20:48:01 +0300

    styled dialogs

--- src/ui/Dialogs/Dialogs.tsx
+++ src/ui/Dialogs/Dialogs.tsx
@@ -1,7 +1,21 @@
 import React from 'react';
+import styles from './dialogs.module.css'
 
 export const Dialogs = () => {
     return (
-        <div>dialogs</div>
+        <div className={styles.dialogs}>
+            <div className={styles.dialogsItem}>
+                <div className={`${styles.dialog}`}>Jenya</div>
+                <div className={styles.dialog}>Sasha</div>
+                <div className={`${styles.dialog} ${styles.active}`}>Dima</div>
+                <div className={styles.dialog}>Rita</div>
+            </div>
+            <div className={styles.messages}>
+                <div className={styles.message}>Hello</div>
+                <div className={styles.message}>How are you?</div>
+                <div className={styles.message}>let's go</div>
+                <div className={styles.message}>Stop</div>
+            </div>
+        </div>
     );
 };
