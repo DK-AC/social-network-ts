95309afcaa17d3291461fe7ef9250be128236955
DK-AC <denkacaj@gmail.com>
2022-04-12 11:46:10 +0300

    useRef input

--- src/ui/Profile/Posts/Posts.tsx
+++ src/ui/Profile/Posts/Posts.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useRef} from 'react';
 import {Post} from "./Post/Post";
 import styles from "./posts.module.css";
 import {PostsDataType} from "../../../redux/state";
@@ -7,17 +7,24 @@ type PropsType = { posts: PostsDataType[] }
 
 export const Posts = ({posts}: PropsType) => {
 
+    const inputEl = useRef<HTMLInputElement>(null)
+
     const post = posts.map(p => {
         return <Post key={p.id} id={p.id} message={p.message} likesCount={p.likesCount}/>
     })
+    const addPostHandle = () => {
+        if (inputEl && inputEl.current) {
+            alert(inputEl.current.value)
+        }
+    }
 
     return (
         <div className={styles.posts}>
             <h3>My posts</h3>
             <div>
                 <div>
-                    <input type="text" placeholder={'post'}/>
-                    <button>Add post</button>
+                    <input ref={inputEl} placeholder={'post'}/>
+                    <button onClick={addPostHandle}>Add post</button>
                 </div>
             </div>
             {post}
