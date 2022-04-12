d68bf48fa68c93cf32117ef1961a48e5df92d102
DK-AC <denkacaj@gmail.com>
2022-04-12 11:03:33 +0300

    PATH const

--- src/ui/Routing/Routing.tsx
+++ src/ui/Routing/Routing.tsx
@@ -11,20 +11,33 @@ import {StateType} from "../../redux/state";
 
 type PropsType = { state: StateType }
 
+export const PATH = {
+    START_PAGE: '/',
+    PROFILE_PAGE: '/profile',
+    DIALOGS_PAGE: '/dialogs',
+    NEWS_PAGE: '/news',
+    MUSIC_PAGE: '/music',
+    SETTINGS_PAGE: '/settings',
+    FRIENDS_PAGE: '/friends',
+    NOT_FOUND_PAGE: '/*'
+}
+
 
 export const Routing = ({state}: PropsType) => {
     return (
         <>
             <Routes>
-                <Route path="/" element={<Profile posts={state.profilePage.posts}/>}/>
-                <Route path="/profile" element={<Profile posts={state.profilePage.posts}/>}/>
-                <Route path="/dialogs/*" element={<Dialogs dialogs={state.dialogsPage.dialogs}
-                                                           messages={state.dialogsPage.messages}/>}/>
-                <Route path="/news" element={<News/>}/>
-                <Route path="/music" element={<Music/>}/>
-                <Route path="/settings" element={<Settings/>}/>
-                <Route path="/friends" element={<Friends friends={state.sideBarPage.friends}/>}/>
-                <Route path="/*" element={<NotFound404/>}/>
+                <Route path={PATH.START_PAGE} element={<Profile posts={state.profilePage.posts}/>}/>
+                <Route path={PATH.PROFILE_PAGE} element={<Profile posts={state.profilePage.posts}/>}/>
+                <Route path={`${PATH.DIALOGS_PAGE}/*`}
+                       element={<Dialogs dialogs={state.dialogsPage.dialogs}
+                                         messages={state.dialogsPage.messages}/>
+                       }/>
+                <Route path={PATH.NEWS_PAGE} element={<News/>}/>
+                <Route path={PATH.MUSIC_PAGE} element={<Music/>}/>
+                <Route path={PATH.SETTINGS_PAGE} element={<Settings/>}/>
+                <Route path={PATH.FRIENDS_PAGE} element={<Friends friends={state.sideBarPage.friends}/>}/>
+                <Route path={PATH.NOT_FOUND_PAGE} element={<NotFound404/>}/>
             </Routes>
         </>
     )
