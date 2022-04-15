746de2344a3fef2e7b8a6d4a9de0dbe7fc5fb84e
DK-AC <denkacaj@gmail.com>
2022-04-15 18:35:46 +0300

    active NavLink dialogs

--- src/ui/Dialogs/DialogItem/DialogItem.tsx
+++ src/ui/Dialogs/DialogItem/DialogItem.tsx
@@ -6,6 +6,17 @@ import dialogPhoto from './../../../assets/img/dialogPhoto1.png'
 type PropsType = { id: number, name: string }
 
 export const DialogItem: React.FC<PropsType> = ({id, name}) => {
+
+    let activeStyle = {
+        color: 'gold',
+        fontWeight: 'bold',
+        textDecoration: 'none'
+    }
+    let defaultStyle = {
+        color: 'white',
+        textDecoration: 'none'
+    }
+
     return (
         <div>
             <span className={styles.dialogItem}>
@@ -13,7 +24,10 @@ export const DialogItem: React.FC<PropsType> = ({id, name}) => {
                 src={dialogPhoto}
                 alt="dialogPhoto"/>
                 </span>
-            <NavLink to={`/dialogs/${id}`} className={`${styles.dialog}`}>{name}</NavLink>
+            <NavLink to={`/dialogs/${id}`}
+                     style={({isActive}) => isActive ? activeStyle : defaultStyle}>
+                {name}
+            </NavLink>
         </div>
     )
 }
