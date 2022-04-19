c0335549a2e0ca9309e0b7d5abfba76c2c4c8e2e
DK-AC <denkacaj@gmail.com>
2022-04-19 21:41:47 +0300

    avatar user

--- src/ui/Header/Header.tsx
+++ src/ui/Header/Header.tsx
@@ -1,12 +1,23 @@
 import React from 'react';
 import styles from './header.module.css'
+import {useAppSelector} from "../../redux/store";
 
 export const Header = () => {
+
+    const isInitialized = useAppSelector<boolean>(state => state.app.isInitialized)
+
     return (
         <header className={styles.header}>
             <img className={styles.logo}
-                src="https://lh3.googleusercontent.com/a-/AOh14Ggy8_OSY8VRciCDIW_ZvzviDGM26X33SG0K0TrlqQ=s600-k-no-rp-mo"
-                alt="logo"/>
+                 src="https://lh3.googleusercontent.com/a-/AOh14Ggy8_OSY8VRciCDIW_ZvzviDGM26X33SG0K0TrlqQ=s600-k-no-rp-mo"
+                 alt="logo"/>
+            {isInitialized ?
+                <img className={styles.initializedUser}
+                     src="https://cdn-icons-png.flaticon.com/512/149/149071.png"
+                     alt="initializedUser"
+                />
+                : ''}
+
         </header>
     );
 };
