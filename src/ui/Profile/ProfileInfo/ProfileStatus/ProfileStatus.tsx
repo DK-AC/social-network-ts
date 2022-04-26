d16836a5c78b765e4489d9f9d0e36e5fdedbf251
DK-AC <denkacaj@gmail.com>
2022-04-26 17:43:12 +0300

    ProfileStatus edit mode

--- src/ui/Profile/ProfileInfo/ProfileStatus/ProfileStatus.tsx
+++ src/ui/Profile/ProfileInfo/ProfileStatus/ProfileStatus.tsx
@@ -1,13 +1,33 @@
-import React from 'react';
+import React, {ChangeEvent, useState} from 'react';
 
 type PropsType = {
     aboutMe: string,
 }
 
 export const ProfileStatus: React.FC<PropsType> = ({aboutMe}) => {
+
+    const [editMode, setEditMode] = useState(false)
+    const [value, setValue] = useState<string>(aboutMe)
+
+    const changeOnEditModeHandle = () => {
+        setValue(value)
+        setEditMode(true)
+    }
+    const changeOnViewModeHandle = () => {
+        setValue(value)
+        setEditMode(false)
+    }
+    const onChangeValueHandle = (e: ChangeEvent<HTMLInputElement>) => {
+        setValue(e.currentTarget.value)
+    }
+
+
     return (
         <>
-            <span>{aboutMe}</span>
+            {editMode
+                ? <input value={value} onBlur={changeOnViewModeHandle} onChange={onChangeValueHandle}/>
+                : <span onDoubleClick={changeOnEditModeHandle}>{value}</span>
+            }
         </>
     );
 };
