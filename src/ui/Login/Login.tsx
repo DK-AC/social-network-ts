fbdc7ade07ada34423543aa25c9b322de8bb64eb
DK-AC <denkacaj@gmail.com>
2022-04-28 10:19:04 +0300

    Login page refactoring

--- src/ui/Login/Login.tsx
+++ src/ui/Login/Login.tsx
@@ -1,10 +1,11 @@
 import 'react-app-polyfill/ie11';
 import * as React from 'react';
 import {useEffect} from 'react';
-import {useFormik} from 'formik';
+import {ErrorMessage, Field, Form, Formik} from 'formik';
 import {useDispatch} from 'react-redux';
 import {useNavigate} from 'react-router-dom';
 import * as Yup from 'yup';
+
 
 import {loginTC} from '../../redux/reducers/authReducer';
 import {useAppSelector} from '../../redux/store';
@@ -24,22 +25,6 @@ export const Login = () => {
 
     const isInitialized = useAppSelector(state => state.auth.isInitialized)
 
-    const formik = useFormik({
-        initialValues: {
-            email: '',
-            password: '',
-            rememberMe: false,
-        },
-        validationSchema: Yup.object({
-            email: Yup.string().email('Invalid email address').required('Required'),
-            password: Yup.string()
-                .min(3, 'Must be 3 characters or more')
-                .required('Required'),
-        }),
-        onSubmit: (values: Values) => {
-            dispatch(loginTC(values))
-        },
-    });
 
     useEffect(() => {
         if (isInitialized) {
@@ -51,42 +36,37 @@ export const Login = () => {
     return (
         <div style={{width: '200px', margin: '0 auto'}}>
             <h1>Sign up</h1>
-            <form onSubmit={formik.handleSubmit}>
-                <div>
-                    <label htmlFor="email">Email</label>
-                    <input
-                        id="email"
-                        placeholder="john@acme.com"
-                        type="email"
-                        {...formik.getFieldProps('email')}
-                    />
-                    {formik.touched.email && formik.errors.email ? (
-                        <div>{formik.errors.email}</div>
-                    ) : null}
-                </div>
-                <div>
-                    <label htmlFor="password">Password</label>
-                    <input
-                        id="password"
-                        placeholder="password"
-                        type="password"
-                        {...formik.getFieldProps('password')}
-                    />
-                    {formik.touched.password && formik.errors.password ? (
-                        <div>{formik.errors.password}</div>
-                    ) : null}
-                </div>
-                <div>
-                    <label htmlFor="rememberMe">Remember me?</label>
-                    <input
-                        id="rememberMe"
-                        type="checkbox"
-                        {...formik.getFieldProps('rememberMe')}
-                    />
-                </div>
-                <button type="submit">Sign Up</button>
-            </form>
+            <Formik
+                initialValues={{email: '', password: '', rememberMe: false}}
+                validationSchema={Yup.object({
+                    email: Yup.string().email('Invalid email address').required('Required'),
+                    password: Yup.string()
+                        .min(3, 'password is short')
+                        .required('Required'),
+                })}
+                onSubmit={(values: Values) => {
+                    dispatch(loginTC(values))
+                }}
+            >
+                <Form>
+                    <div>
+                        <label htmlFor="email">Email</label>
+                        <Field name="email" placeholder="john@acme.com" type="email"/>
+                        <ErrorMessage name="email"/>
+                    </div>
+                    <div>
+                        <label htmlFor="password">Password</label>
+                        <Field name="password" placeholder="password" type="password"/>
+                        <ErrorMessage name="password"/>
+                    </div>
+                    <div>
+                        <label htmlFor="rememberMe">Remember me?</label>
+                        <Field name="rememberMe" type="checkbox"/>
+                    </div>
+                    <button type="submit">Sign Up</button>
+                </Form>
+            </Formik>
         </div>
-    );
+    )
 };
 
