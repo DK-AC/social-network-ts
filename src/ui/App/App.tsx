6c3e86ebc384d33225e6940e29a09ddcf1724960
DK-AC <denkacaj@gmail.com>
2022-04-29 12:18:34 +0300

    fix navigate to login

--- src/ui/App/App.tsx
+++ src/ui/App/App.tsx
@@ -1,21 +1,29 @@
 import {useDispatch} from 'react-redux';
 import React, {useEffect} from 'react';
+import {useNavigate} from 'react-router-dom';
 
 import {Header} from '../Header/Header';
 import {Navbar} from '../Navbar/Navbar';
-import {Routing} from '../Routing/Routing';
+import {PATH, Routing} from '../Routing/Routing';
 import {authMeTC} from '../../redux/reducers/authReducer';
+import {useAppSelector} from '../../redux/store';
 
 import styles from './App.module.css';
 
-
 export const App: React.FC = () => {
 
-    const dispatch = useDispatch();
+    const dispatch = useDispatch()
+    const navigate = useNavigate()
+
+    const isInitialized = useAppSelector(state => state.auth.isInitialized)
 
     useEffect(() => {
-        dispatch(authMeTC());
-    }, []);
+        if (!isInitialized) {
+            dispatch(authMeTC());
+            navigate(PATH.LOGIN_PAGE)
+        }
+        return
+    }, [isInitialized]);
 
     return (
         <div className={styles.appWrapper}><Header/>
