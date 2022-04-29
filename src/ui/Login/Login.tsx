7bfb1bff57106e25858ebdb8be2bc0ee83728551
DK-AC <denkacaj@gmail.com>
2022-04-29 08:59:02 +0300

    refactoring Login

--- src/ui/Login/Login.tsx
+++ src/ui/Login/Login.tsx
@@ -9,7 +9,6 @@ import * as Yup from 'yup';
 import {loginTC} from '../../redux/reducers/authReducer';
 import {useAppSelector} from '../../redux/store';
 import {PATH} from '../Routing/Routing';
-import {FieldFormik} from '../../ReusableComponent/Formik/FieldFormik';
 
 type Values = { email: string, password: string, rememberMe: boolean }
 
@@ -20,6 +19,15 @@ export const Login = () => {
     const navigate = useNavigate()
 
     const isInitialized = useAppSelector(state => state.auth.isInitialized)
+
+    const initialValues = {email: '', password: '', rememberMe: false}
+    const validationSchema = {
+        email: Yup.string().email('Invalid email format').required('Required'),
+        password: Yup.string().required('Required'),
+    }
+    const onSubmitLoginUser = (values: Values) => {
+        dispatch(loginTC(values))
+    }
 
     useEffect(() => {
         if (isInitialized) {
@@ -32,23 +40,29 @@ export const Login = () => {
         <div style={{width: '200px', margin: '0 auto'}}>
             <h1>Sign up</h1>
             <Formik
-                initialValues={{email: '', password: '', rememberMe: false}}
-                validationSchema={Yup.object({
-                    email: Yup.string().email('Invalid email address').required('Required'),
-                    password: Yup.string()
-                        .min(3, 'password is short')
-                        .required('Required'),
-                })}
-                onSubmit={(values: Values, actions) => {
-                    dispatch(loginTC(values))
-                }}
+                initialValues={initialValues}
+                validationSchema={Yup.object(validationSchema)}
+                onSubmit={onSubmitLoginUser}
             >
-                <Form>
-                    <FieldFormik type={'email'} name={'email'}/>
-                    <FieldFormik type={'password'} name={'password'}/>
-                    <FieldFormik type={'checkbox'} name={'rememberMe'}/>
-                    <button type="submit">Sign Up</button>
-                </Form>
+                {formik => (
+                    <Form>
+                        <div>
+                            <label htmlFor={'email'}>email</label>
+                            <Field name={'email'} type={'email'}/>
+                            <ErrorMessage name={'email'}/>
+                        </div>
+                        <div>
+                            <label htmlFor={'password'}>password</label>
+                            <Field name={'password'} type={'password'}/>
+                            <ErrorMessage name={'password'}/>
+                        </div>
+                        <div>
+                            <label htmlFor={'rememberMe'}>remember me?</label>
+                            <Field name={'rememberMe'} type={'checkbox'}/>
+                        </div>
+                        <button disabled={!formik.isValid} type="submit">Sign Up</button>
+                    </Form>
+                )}
             </Formik>
         </div>
     )
