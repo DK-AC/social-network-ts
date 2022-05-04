02ea62dccdcf96cc802b4b275fcb3a4c951bc90e
DK-AC <denkacaj@gmail.com>
2022-05-04 12:43:07 +0300

    refactoring user set test

--- src/redux/tests/usersReducer.test.ts
+++ src/redux/tests/usersReducer.test.ts
@@ -1,5 +1,6 @@
 import {
     changeCurrentPageAC,
+    followingInProgressAC,
     followUserAC,
     setTotalUserCountAC,
     setUsersAC,
@@ -8,28 +9,11 @@ import {
     usersReducer,
 } from '../reducers/usersReducer';
 
-let startState: UsersPageType;
+let fakeState: UsersPageType;
 
 beforeEach(() => {
-        startState = {
-            users: [
-                {
-                    id: 1,
-                    name: 'Denis',
-                    uniqueUrlName: '',
-                    photos: {small: '', large: ''},
-                    status: '',
-                    followed: true,
-                },
-                {
-                    id: 2,
-                    name: 'Jenya',
-                    uniqueUrlName: '',
-                    photos: {small: '', large: ''},
-                    status: '',
-                    followed: false,
-                },
-            ],
+        fakeState = {
+            users: [],
             totalCount: 100,
             pageSize: 5,
             currentPage: 3,
@@ -40,38 +24,31 @@ beforeEach(() => {
 
 test('unfollowed user should be changed on true', () => {
 
-    const endState = usersReducer(startState, followUserAC(2));
+    const endState = usersReducer(fakeState, followUserAC(2));
 
-    expect(startState.users[1].followed).toBeFalsy();
+    expect(fakeState.users[1].followed).toBeFalsy();
     expect(endState.users[1].followed).toBeTruthy();
 });
 
 test('followed user should be changed on false', () => {
 
-    const endState = usersReducer(startState, unfollowUserAC(1));
+    const endState = usersReducer(fakeState, unfollowUserAC(1));
 
-    expect(startState.users[0].followed).toBeTruthy();
+    expect(fakeState.users[0].followed).toBeTruthy();
     expect(endState.users[0].followed).toBeFalsy();
 });
 
 test('users should be sets', () => {
 
-    const fakeState: UsersPageType = {users: [], totalCount: 1, currentPage: 1, pageSize: 5, followingInProgress: []};
-
-    const endState = usersReducer(fakeState, setUsersAC(startState.users));
-
-    expect(fakeState).toEqual({users: [], totalCount: 1, currentPage: 1, pageSize: 5, followingInProgress: []});
-    expect(fakeState.users.length).toBe(0);
-    expect(endState).toEqual({
-        users: [
-            {
-                id: 1,
-                name: 'Denis',
-                uniqueUrlName: '',
-                photos: {small: '', large: ''},
-                status: '',
-                followed: true,
-            },
+    const fakeUserState: UsersPageType = {
+        users: [{
+            id: 1,
+            name: 'Denis',
+            uniqueUrlName: '',
+            photos: {small: '', large: ''},
+            status: '',
+            followed: true,
+        },
             {
                 id: 2,
                 name: 'Jenya',
@@ -79,26 +56,67 @@ test('users should be sets', () => {
                 photos: {small: '', large: ''},
                 status: '',
                 followed: false,
-            },
-        ],
-        totalCount: 1,
+            }],
+        totalCount: 100,
+        currentPage: 3,
         pageSize: 5,
-        currentPage: 1,
         followingInProgress: [],
+    };
+
+    const endState = usersReducer(fakeState, setUsersAC(fakeUserState.users));
+
+    expect(fakeState).toEqual({
+        users: [],
+        totalCount: 100,
+        currentPage: 3,
+        pageSize: 5,
+        followingInProgress: [],
+    });
+    expect(fakeState.users.length).toBe(0);
+    expect(endState).toEqual({
+        totalCount: 100,
+        pageSize: 5,
+        currentPage: 3,
+        followingInProgress: [],
+        users: [{
+            id: 1,
+            name: 'Denis',
+            uniqueUrlName: '',
+            photos: {small: '', large: ''},
+            status: '',
+            followed: true,
+        },
+            {
+                id: 2,
+                name: 'Jenya',
+                uniqueUrlName: '',
+                photos: {small: '', large: ''},
+                status: '',
+                followed: false,
+            }],
     });
     expect(endState.users.length).toBe(2);
 });
 
 test('total user count should be set', () => {
-    const endState = usersReducer(startState, setTotalUserCountAC(4000));
+    const endState = usersReducer(fakeState, setTotalUserCountAC(4000));
 
-    expect(startState.totalCount).toBe(100);
+    expect(fakeState.totalCount).toBe(100);
     expect(endState.totalCount).toBe(4000);
 });
 
 test('page should be changed', () => {
-    const endState = usersReducer(startState, changeCurrentPageAC(10));
+    const endState = usersReducer(fakeState, changeCurrentPageAC(10));
 
-    expect(startState.currentPage).toBe(3);
+    expect(fakeState.currentPage).toBe(3);
     expect(endState.currentPage).toBe(10);
+});
+
+test('show following user', () => {
+
+    const userId1 = 1
+    const endState = usersReducer(fakeState, followingInProgressAC(false, userId1));
+
+    expect(fakeState.followingInProgress.some(userId => userId === userId1)).toBe(false)
+    expect(endState.followingInProgress.some(userId => userId === userId1)).toBe(false)
 });
