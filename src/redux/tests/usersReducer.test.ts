3c8cbda3b2a8dc57cf9e20067fc0cb43c691d0cd
DK-AC <denkacaj@gmail.com>
2022-05-11 15:14:20 +0300

    user test refactoring

--- src/redux/tests/usersReducer.test.ts
+++ src/redux/tests/usersReducer.test.ts
@@ -1,7 +1,9 @@
 import {
+    changeCurrentPage,
     changePortionNumber,
     followingTC,
     InitialUsersStateType,
+    setIsFollowingInProgress,
     setUsersTC,
     usersReducer,
 } from '../reducers/usersReducer';
@@ -37,101 +39,101 @@ beforeEach(() => {
     },
 );
 
-test('unfollowed user should be changed on true', () => {
+describe('user', () => {
+    test('unfollowed user should be changed on true', () => {
 
-    const action = followingTC
-        .fulfilled({isFollow: true, userId: 2}, 'requestId', {isFollow: true, userId: 2})
-    const endState = usersReducer(fakeState, action);
+        const action = followingTC
+            .fulfilled({isFollow: true, userId: 2}, 'requestId', {isFollow: true, userId: 2})
+        const endState = usersReducer(fakeState, action);
 
-    expect(fakeState.users[1].followed).toBeFalsy();
-    expect(endState.users[1].followed).toBeTruthy();
-});
+        expect(fakeState.users[1].followed).toBeFalsy();
+        expect(endState.users[1].followed).toBeTruthy();
+    });
 
-test('followed user should be changed on false', () => {
+    test('followed user should be changed on false', () => {
 
-    const action = followingTC
-        .fulfilled({isFollow: false, userId: 1}, 'requestId', {isFollow: false, userId: 1})
-    const endState = usersReducer(fakeState, action);
+        const action = followingTC
+            .fulfilled({isFollow: false, userId: 1}, 'requestId', {isFollow: false, userId: 1})
+        const endState = usersReducer(fakeState, action);
 
-    expect(fakeState.users[0].followed).toBeTruthy();
-    expect(endState.users[0].followed).toBeFalsy();
-});
+        expect(fakeState.users[0].followed).toBeTruthy();
+        expect(endState.users[0].followed).toBeFalsy();
+    });
 
-test('users should be sets', () => {
+    test('users should be sets', () => {
 
-    const fakeUsersForTests = [
-        {
-            id: 3,
-            name: 'Sasha',
-            uniqueUrlName: '',
-            photos: {small: '', large: ''},
-            status: '',
-            followed: true,
-        },
-        {
-            id: 4,
-            name: 'Rita',
-            uniqueUrlName: '',
-            photos: {small: '', large: ''},
-            status: '',
-            followed: false,
-        }];
-    const fakeUserState = {
-        users: fakeUsersForTests,
-        totalCount: 100,
-        currentPage: 3,
-        pageSize: 5,
-        followingInProgress: [],
-        portionNumber: 1,
-    };
+        const fakeUsersForTests = [
+            {
+                id: 3,
+                name: 'Sasha',
+                uniqueUrlName: '',
+                photos: {small: '', large: ''},
+                status: '',
+                followed: true,
+            },
+            {
+                id: 4,
+                name: 'Rita',
+                uniqueUrlName: '',
+                photos: {small: '', large: ''},
+                status: '',
+                followed: false,
+            }];
+        const fakeUserState = {
+            users: fakeUsersForTests,
+            totalCount: 4000,
+            currentPage: 3,
+            pageSize: 5,
+            followingInProgress: [],
+            portionNumber: 1,
+        };
 
-    const action = setUsersTC.fulfilled({
-        users: fakeUserState.users,
-        totalCount: fakeUserState.totalCount,
-    }, 'requestId', {currentPage: 3, pageSize: 5})
+        const action = setUsersTC.fulfilled({
+            users: fakeUserState.users,
+            totalCount: fakeUserState.totalCount,
+        }, 'requestId', {currentPage: 3, pageSize: 5})
 
-    const endState = usersReducer(fakeState, action);
+        const endState = usersReducer(fakeState, action);
 
-    expect(fakeState).toEqual(fakeState);
-    expect(fakeState.users.length).toBe(2);
-    expect(endState).toEqual(fakeUserState);
-    expect(endState.users.length).toBe(2);
-});
-//
-// test('total user count should be set', () => {
-//     const endState = usersReducer(fakeState, setTotalUserCountAC(4000));
-//
-//     expect(fakeState.totalCount).toBe(100);
-//     expect(endState.totalCount).toBe(4000);
-// });
-//
-// test('page should be changed', () => {
-//     const endState = usersReducer_(fakeState, changeCurrentPage({currentPage: 10}));
-//
-//     expect(fakeState.currentPage).toBe(3);
-//     expect(endState.currentPage).toBe(10);
-// });
+        expect(fakeState).toEqual(fakeState);
+        expect(fakeState.users.length).toBe(2);
 
-// test('show following user', () => {
-//
-//     const userId1 = 1
-//     const endState = usersReducer(fakeState, followingInProgressAC(false, userId1));
-//
-//     expect(fakeState.followingInProgress.some(userId => userId === userId1)).toBe(false)
-//     expect(endState.followingInProgress.some(userId => userId === userId1)).toBe(false)
-// });
-//
-// test('show unfollowing user', () => {
-//
-//     const userId1 = 2
-//     const endState = usersReducer(fakeState, followingInProgressAC(true, userId1));
-//
-//     expect(fakeState.followingInProgress.some(userId => userId === userId1)).toBe(false)
-//     expect(endState.followingInProgress.some(userId => userId === userId1)).toBe(true)
-// });
-test('portionNumber should be changed', () => {
-    const endState = usersReducer(fakeState, changePortionNumber({portionNumber: 4}))
+        expect(endState).toEqual(fakeUserState);
+        expect(endState.users.length).toBe(2);
 
-    expect(fakeState.portionNumber).toBe(1);
-    expect(endState.portionNumber).toBe(4);
+        expect(fakeState.totalCount).toBe(100);
+        expect(endState.totalCount).toBe(4000);
+    });
+
+    test('page should be changed', () => {
+        const endState = usersReducer(fakeState, changeCurrentPage({currentPage: 10}));
+
+        expect(fakeState.currentPage).toBe(3);
+        expect(endState.currentPage).toBe(10);
+    });
+
+    test('show following user', () => {
+
+        const userId1 = 1
+        const endState = usersReducer(fakeState, setIsFollowingInProgress({isFollow: false, userId: userId1}));
+
+        expect(fakeState.followingInProgress.some(userId => userId === userId1)).toBe(false)
+        expect(endState.followingInProgress.some(userId => userId === userId1)).toBe(false)
+    });
+
+    test('show unfollowing user', () => {
+
+        const userId2 = 2
+        const endState = usersReducer(fakeState, setIsFollowingInProgress({isFollow: true, userId: userId2}));
+
+        expect(fakeState.followingInProgress.some(userId => userId === userId2)).toBe(false)
+        expect(endState.followingInProgress.some(userId => userId === userId2)).toBe(true)
+    });
+    test('portionNumber should be changed', () => {
+        const endState = usersReducer(fakeState, changePortionNumber({portionNumber: 4}))
+
+        expect(fakeState.portionNumber).toBe(1);
+        expect(endState.portionNumber).toBe(4);
+    })
 })
+
