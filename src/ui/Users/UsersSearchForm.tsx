20476443a7ecd6823945de1bd96efd4590c2b2cd
DK-AC <denkacaj@gmail.com>
2022-05-20 22:42:24 +0300

    search users

--- src/ui/Users/UsersSearchForm.tsx
+++ src/ui/Users/UsersSearchForm.tsx
@@ -5,17 +5,21 @@ import {useDispatch} from 'react-redux'
 
 import {FormikField} from '../../reusableComponent/FormikField'
 import {useAppSelector} from '../../redux/store'
+import {setUsers} from '../../redux/reducers/usersReducer'
+import {ParamsUserPageType} from '../../api/userAPI'
 
 export const UsersSearchForm = () => {
 
     const dispatch = useDispatch()
 
+    const {currentPage, pageSize} = useAppSelector(state => state.users)
+    const {term} = useAppSelector(state => state.users.filter)
     const {error} = useAppSelector(state => state.app)
 
-    const initialValues = {term: ''}
+    const initialValues = {term, currentPage, pageSize}
     const validationSchema = {}
-    const onSubmitLoginUser = () => {
-        // dispatch(login(values))
+    const onSubmitLoginUser = (values: ParamsUserPageType) => {
+        dispatch(setUsers({pageSize, currentPage, term: values.term}))
     }
 
     return (
