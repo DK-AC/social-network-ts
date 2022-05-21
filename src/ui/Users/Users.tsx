b733cb4817e09343ce456b3f6ff4852882068d8f
DK-AC <denkacaj@gmail.com>
2022-05-21 23:38:48 +0300

    update uri params

--- src/ui/Users/Users.tsx
+++ src/ui/Users/Users.tsx
@@ -1,21 +1,32 @@
 import React, {useEffect} from 'react'
 import {useDispatch} from 'react-redux'
-import {useNavigate} from 'react-router-dom'
+import {useNavigate, useSearchParams} from 'react-router-dom'
 
 import {useAppSelector} from '../../redux/store'
-import {setUsers} from '../../redux/reducers/usersReducer'
 import {Preloader} from '../Preloader/Preloader'
 import {PATH} from '../Routing/Routing'
 import {Paginator} from '../common/Paginator'
+import {changeCurrentPage, setUsers} from '../../redux/reducers/usersReducer'
 
 import {User} from './User/User'
 import styles from './users.module.css'
 import {UsersSearchForm} from './UsersSearchForm'
 
-export const Users: React.FC = () => {
+type TestType = {
+    page: string
+    count: string
+    term: string
+    friend: string
+}
 
+export const Users: React.FC = () => {
     const dispatch = useDispatch()
     const navigate = useNavigate()
+    const [searchParams, setSearchParams] = useSearchParams()
+
+    //const test = {page: 1, count: 10, term: '', friend: null}
+    //Object.fromEntries({...searchParams})
+    //console.log(searchParams.get('friend'))
 
     const {users, currentPage, pageSize} = useAppSelector(state => state.users)
     const {term, friend} = useAppSelector(state => state.users.filter)
@@ -26,28 +37,130 @@ export const Users: React.FC = () => {
         return <User key={u.id} user={u}/>
     })
 
-    useEffect(() => {
-        navigate({
-            pathname: '/users',
-            search: `?page=${currentPage}&count=${pageSize}&term=${term}&friend=${friend}`,
-        })
-    }, [pageSize, currentPage, term, friend, navigate])
+    const pageQ = searchParams.get('page') || 'c'
+    const countQ = searchParams.get('count') || '10'
+    const termQ = searchParams.get('term') || ''
+    const friendQ = searchParams.get('friend') || 'null'
+
+    const params = {
+        page: pageQ,
+        count: countQ,
+        term: termQ,
+        friend: friendQ,
+    }
 
     useEffect(() => {
-        if (!isAuth) {
-            navigate(PATH.LOGIN_PAGE)
-        }
-        dispatch(setUsers({pageSize, currentPage, term, friend}))
-    }, [isAuth, navigate, currentPage])
+        setSearchParams(params)
+        console.log(params)
+
+        dispatch(setUsers({
+            pageSize: +params.count,
+            currentPage: +params.page,
+            term: params.term,
+            friend: params.friend === 'null' ? null : params.friend === 'true',
+        }))
+        dispatch(changeCurrentPage({currentPage: +params.page}))
+
+    }, [dispatch, pageQ,params.page])
+
+    // useEffect(() => {
+    //         navigate({
+    //             pathname: '/users',
+    //             search: createSearchParams(params).toString(),
+    //         })
+    //     }
+    //     , [])
+
+
+    if (!isAuth) {
+        navigate(PATH.LOGIN_PAGE)
+    }
 
     return (
         <div className={styles.userItems}>
             {status === 'loading' ? <Preloader/>
                 : <div>
-                    <Paginator/>
+                    <Paginator setSearchParams={setSearchParams}/>
                     <UsersSearchForm/>
                     {user}
                 </div>}
         </div>
     )
 }
+
+
+// import React, {useEffect} from 'react'
+// import {useDispatch} from 'react-redux'
+// import {useNavigate, useSearchParams} from 'react-router-dom'
+//
+// import {useAppSelector} from '../../redux/store'
+// import {setUsers} from '../../redux/reducers/usersReducer'
+// import {Preloader} from '../Preloader/Preloader'
+// import {PATH} from '../Routing/Routing'
+// import {Paginator} from '../common/Paginator'
+//
+// import {User} from './User/User'
+// import styles from './users.module.css'
+// import {UsersSearchForm} from './UsersSearchForm'
+//
+// export const Users: React.FC = () => {
+//
+//     const dispatch = useDispatch()
+//     const navigate = useNavigate()
+//
+//     const [searchParams] = useSearchParams()
+//
+//
+//
+//
+//
+//     const {users, currentPage, pageSize} = useAppSelector(state => state.users)
+//     const {term, friend} = useAppSelector(state => state.users.filter)
+//     const {status} = useAppSelector(state => state.app)
+//     const isAuth = useAppSelector(state => state.auth.isAuth)
+//
+//     const user = users.map(u => {
+//         return <User key={u.id} user={u}/>
+//     })
+//
+//     useEffect(() => {
+//         navigate({
+//             pathname: '/users',
+//             search: `?page=${currentPage}&count=${pageSize}&term=${term}&friend=${friend}`,
+//         })
+//     }, [pageSize, currentPage, term, friend, navigate])
+//
+//     useEffect(() => {
+//         // @ts-ignore
+//         const currentParams = Object.fromEntries([...searchParams])
+//         dispatch(setUsers({
+//             pageSize: currentParams.count,
+//             currentPage: currentParams.page,
+//             term: currentParams.term,
+//             friend: currentParams.friend,
+//         }))
+//         console.log(currentParams) // get new values onchange
+//     }, [searchParams])
+//
+//
+//
+//     useEffect(() => {
+//
+//         // dispatch(setUsers({pageSize, currentPage, term, friend}))
+//     }, [isAuth, navigate, currentPage])
+//
+//     if (!isAuth) {
+//         navigate(PATH.LOGIN_PAGE)
+//     }
+//
+//     return (
+//         <div className={styles.userItems}>
+//             {status === 'loading' ? <Preloader/>
+//                 : <div>
+//                     <Paginator/>
+//                     <UsersSearchForm/>
+//                     {user}
+//                 </div>}
+//         </div>
+//     )
+// }
--- src/ui/common/Paginator.tsx
+++ src/ui/common/Paginator.tsx
@@ -6,9 +6,12 @@ import {useAppSelector} from '../../redux/store'
 
 import styles from './paginator.module.css'
 
-type PropsType = { portionSize?: number }
+type PropsType = {
+    portionSize?: number
+    setSearchParams: any
+}
 
-export const Paginator: React.FC<PropsType> = ({portionSize = 10}) => {
+export const Paginator: React.FC<PropsType> = ({portionSize = 10, setSearchParams}) => {
 
     const dispatch = useDispatch()
 
@@ -43,7 +46,10 @@ export const Paginator: React.FC<PropsType> = ({portionSize = 10}) => {
                     return (
                         <span key={index}
                               className={currentPage === p ? styles.selectedPage : '' + styles.pageNumber}
-                              onClick={() => dispatch(changeCurrentPage({currentPage: p}))}
+                              onClick={() => {
+                                  setSearchParams({page: p})
+                                  dispatch(changeCurrentPage({currentPage: p}))
+                              }}
                         >
                         {p}
                     </span>)
@@ -58,3 +64,65 @@ export const Paginator: React.FC<PropsType> = ({portionSize = 10}) => {
 
 
 
+
+
+// import React from 'react'
+// import {useDispatch} from 'react-redux'
+//
+// import {changeCurrentPage, changePortionNumber} from '../../redux/reducers/usersReducer'
+// import {useAppSelector} from '../../redux/store'
+//
+// import styles from './paginator.module.css'
+//
+// type PropsType = { portionSize?: number }
+//
+// export const Paginator: React.FC<PropsType> = ({portionSize = 10}) => {
+//
+//     const dispatch = useDispatch()
+//
+//     const {pageSize, currentPage, totalCount, portionNumber} = useAppSelector(state => state.users)
+//
+//     const pagesCount = Math.ceil(totalCount / pageSize)
+//     const portionCount = Math.ceil(pagesCount / portionSize)
+//     const leftPortionPageNumber = (portionNumber - 1) * portionSize + 1
+//     const rightPortionPageNumber = portionNumber * portionSize
+//
+//     const pages = []
+//     for (let i = 1; i <= pagesCount; i++) {
+//         if (i === totalCount) break
+//         pages.push(i)
+//     }
+//
+//     const setPortionMinusNumberHandle = () => {
+//         dispatch(changePortionNumber({portionNumber: portionNumber - 1}))
+//     }
+//     const setPortionPlusNumberHandle = () => {
+//         dispatch(changePortionNumber({portionNumber: portionNumber + 1}))
+//     }
+//
+//     return (
+//         <div className={styles.paginator}>
+//
+//             {portionNumber > 1 && <button onClick={setPortionMinusNumberHandle}>PREV</button>}
+//
+//             {pages
+//                 .filter(p => p >= leftPortionPageNumber && p <= rightPortionPageNumber)
+//                 .map((p, index) => {
+//                     return (
+//                         <span key={index}
+//                               className={currentPage === p ? styles.selectedPage : '' + styles.pageNumber}
+//                               onClick={() => dispatch(changeCurrentPage({currentPage: p}))}
+//                         >
+//                         {p}
+//                     </span>)
+//                 })}
+//
+//             {portionCount > portionNumber && <button onClick={setPortionPlusNumberHandle}>NEXT</button>}
+//
+//         </div>
+//
+//     )
+// }
+//
+//
+//
