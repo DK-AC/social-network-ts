0c6a7aedb8c8b3af4aadcadb3d9470d7af178454
DK-AC <denkacaj@gmail.com>
2022-05-24 17:46:24 +0300

    add layout to app

--- src/ui/App/App.tsx
+++ src/ui/App/App.tsx
@@ -1,17 +1,22 @@
 import {useDispatch} from 'react-redux'
 import React, {useEffect} from 'react'
 import {useNavigate} from 'react-router-dom'
-import {Button} from 'antd'
+import {Layout, Menu, MenuProps} from 'antd'
+import {
+    CustomerServiceOutlined,
+    MessageOutlined,
+    ProfileOutlined,
+    SettingOutlined,
+    TeamOutlined,
+    UsergroupDeleteOutlined,
+} from '@ant-design/icons'
 
-import {Header} from '../Header/Header'
-import {Navbar} from '../Navbar/Navbar'
-import {PATH, Routing} from '../Routing/Routing'
+import {PATH} from '../Routing/Routing'
 import {authMe} from '../../redux/reducers/authReducer'
 import {useAppSelector} from '../../redux/store'
-import {Preloader} from '../Preloader/Preloader'
 
-import styles from './App.module.css'
 import './App.css'
+
 
 export const App: React.FC = () => {
 
@@ -20,6 +25,46 @@ export const App: React.FC = () => {
 
     const isAuth = useAppSelector(state => state.auth.isAuth)
     const isInitialized = useAppSelector(state => state.app.isInitialized)
+
+    const {Header, Content, Footer, Sider} = Layout
+
+    const items: MenuProps['items'] = [
+        {
+            key: '1',
+            label: 'Profile',
+            icon: <ProfileOutlined/>,
+        },
+        {
+            key: '2',
+            label: 'Users',
+            icon: <UsergroupDeleteOutlined/>,
+        },
+        {
+            key: '3',
+            label: 'Dialogs',
+            icon: <MessageOutlined/>,
+        },
+        {
+            key: '4',
+            label: 'News',
+            icon: <MessageOutlined/>,
+        },
+        {
+            key: '5',
+            label: 'Music',
+            icon: <CustomerServiceOutlined/>,
+        },
+        {
+            key: '6',
+            label: 'Settings',
+            icon: <SettingOutlined/>,
+        },
+        {
+            key: '7',
+            label: 'Friends',
+            icon: <TeamOutlined/>,
+        },
+    ]
 
     useEffect(() => {
         dispatch(authMe())
@@ -34,17 +79,42 @@ export const App: React.FC = () => {
     }, [isAuth, isInitialized, navigate])
 
     return (
-        <div className={'App'}>
-            {!isInitialized
-                ? <Preloader/>
-                : <div className={styles.appWrapper}>
-                    <Header/>
-                    <Navbar/>
-                    <div className={styles.appWrapperContent}>
-                        <Routing/>
+        <Layout hasSider>
+            <Sider
+                style={{
+                    overflow: 'auto',
+                    height: '100vh',
+                    position: 'fixed',
+                    left: 0,
+                    top: 0,
+                    bottom: 0,
+                }}
+            >
+                <div className="logo"/>
+                <Menu theme="dark" mode="inline" defaultSelectedKeys={['1']} items={items}/>
+            </Sider>
+            <Layout className="site-layout" style={{marginLeft: 200}}>
+                {/*<Header/>*/}
+                <Header className="site-layout-background" style={{padding: 0}}/>
+                <Content style={{margin: '24px 16px 0', overflow: 'initial'}}>
+                    <div className="site-layout-background" style={{padding: 24, textAlign: 'center'}}>
                     </div>
-                </div>}
-        </div>
+                </Content>
+                <Footer style={{textAlign: 'center'}}>Ant Design ©2018 Created by Ant UED</Footer>
+            </Layout>
+        </Layout>
+        // <div className={'App'}>
+        //     {!isInitialized
+        //         ? <Preloader/>
+        //         : <div className={styles.appWrapper}>
+        //             <Header/>
+        //             <Navbar/>
+        //             <div className={styles.appWrapperContent}>
+        //                 <Routing/>
+        //             </div>
+        //         </div>}
+        //
+        // </div>
     )
 }
 
