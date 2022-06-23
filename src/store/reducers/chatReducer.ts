77a76785692811468e23401135b9ddd63c5b5444
DK-AC <denkacaj@gmail.com>
2022-06-23 19:38:53 +0300

    read react docum

--- src/store/reducers/chatReducer.ts
+++ src/store/reducers/chatReducer.ts
@@ -13,6 +13,7 @@ const initialState = {
 
 console.log('Read react documentation')
 console.log('Read react documentation')
+console.log('Read react documentation')
 
 export const chatSlices = createSlice({
     name: 'chat',
